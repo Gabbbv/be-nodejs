406a07b0212172a2fc26d565ed9a1f3edf5f1ffb
Gabriele Ventura <GABRIELE.VENTURA@local.ittcesena.it>
2026-04-09 09:13:31 +0200

    Finalize Docker build and Prisma configuration

--- src/utils/hashing.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const getPasswordHash: (password: string) => Promise<string>;
-export declare const verifyPassword: (plainPassword: string, hashedPassword: string) => Promise<boolean>;
-//# sourceMappingURL=hashing.d.ts.map
--- src/utils/session.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare const generaToken: (userId: number) => string;
-export declare const autenticaUtente: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
-//# sourceMappingURL=session.d.ts.map
